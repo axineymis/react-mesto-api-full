d9861753ab7406d43465d52514f5b0bf3282b8e9
axineymis <otm-natasha.biryukova@yandex.ru>
2022-05-25 00:35:30 +0300

    revert back backend

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,19 +2,19 @@ const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 require('dotenv').config();
 
-const JWT_SECRET_KEY = 'test';
+const { JWT_SECRET_KEY = 'test' } = process.env;
 
 module.exports = (req, res, next) => {
   const cookieAuthorization = req.cookies.jwt;
-  console.log(jwt.verify(cookieAuthorization, JWT_SECRET_KEY));
-  // console.log('cookieAuthorization =');
-  // console.log(req);
+  console.log('cookieAuthorization =');
+  console.log(req);
   if (!cookieAuthorization) {
     return next(new AuthorizationError('Ошибка авторизации'));
   }
   let payload;
   try {
     payload = jwt.verify(cookieAuthorization, JWT_SECRET_KEY);
+    console.log(jwt.verify);
   } catch (err) {
     return next(new AuthorizationError('Необходима авторизация'));
   }
